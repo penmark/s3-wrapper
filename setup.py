684984d05307c6b5d11d0a663532103c2bedd7b8
Pontus Enmark <pontus@wka.se>
2016-12-14 08:31:00 +0100

    Rename package

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 
-setup(name='S3',
+setup(name='s3_wrapper',
         version='0.0.1',
         description='boto wrapper',
         author='Pontus Enmark',
         author_email='pontus@wka.se',
-        url='https://wka.se/s3',
-        packages=['s3'],
+        url='https://github.com/penmark/s3-wrapper',
+        packages=['s3_wrapper'],
         install_requires=[
             'boto==2.44.0',
             'python-dotenv==0.6.1'
             ],
         entry_points='''
         [console_scripts]
-        s3 = s3.main:main
+        s3 = s3_wrapper.main:main
         '''
         )
