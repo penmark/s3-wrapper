762ffaa7a85e6e838d1e005770713c230d44b5cd
Pontus Enmark <pontus@wka.se>
2016-12-16 08:57:35 +0100

    1.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='s3_wrapper',
-        version='1.0.1',
+        version='1.0.2',
         description='s3 boto wrapper',
         author='Pontus Enmark',
         author_email='pontus@wka.se',
